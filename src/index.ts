47ac18db35e75c36cbcbfaaeb3fe8a5b2a481ec5
Shigma <shigma10826@gmail.com>
2021-12-08 14:08:59 +0800

    chore: migrate schema api

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ export const name = 'dice'
 export interface Config extends RollConfig {}
 
 export const Config = Schema.object({
-  maxPoint: Schema.number('掷骰的最大点数。').default(1 << 16),
-  maxTimes: Schema.number('单次调用中最大掷骰次数。').default(64),
+  maxPoint: Schema.number().description('掷骰的最大点数。').default(1 << 16),
+  maxTimes: Schema.number().description('单次调用中最大掷骰次数。').default(64),
 })
 
 export function apply(ctx: Context, config: Config = {}) {
