141a0e72a03c13496c0b723c1c4327a290e9c121
Anillc <i@anillc.cn>
2023-02-14 08:48:37 +0800

    add description to schema

--- src/onedice.ts
+++ src/onedice.ts
@@ -2,45 +2,50 @@ import { Context, Schema, segment } from 'koishi'
 import { Config as OnediceConfig, dice } from '@onedice/core'
 import { Config } from '.'
 
-export type DiceConfig = OnediceConfig & {
-  inline: boolean
+export type DiceConfig = {
   maxDetailsSize: number
   maxRollTimes: number
+  onedice: OnediceConfig
 }
 
 export const DiceSchema: Schema<DiceConfig> = Schema.object({
-  random: Schema.function().description('随机数生成函数。'),
-  maxRollCount: Schema.number().description('单次投掷最大投掷数量。').min(0).step(1),
   maxRollTimes: Schema.number().description('最大重复投掷次数。').default(10),
   maxDetailsSize: Schema.number().description('过程展示最大字数，超过则省略过程。').default(500),
-  inline: Schema.boolean().default(true),
-  d: Schema.object({
-    a: Schema.number(),
-    b: Schema.number(),
-    c: Schema.number(),
-    d: Schema.number(),
-    e: Schema.number(),
-  }),
-  p: Schema.object({
-    a: Schema.number(),
-    b: Schema.number(),
-  }),
-  a: Schema.object({
-    a: Schema.number(),
-    b: Schema.number(),
-    c: Schema.number(),
-    d: Schema.number(),
-    e: Schema.number(),
-  }),
-  c: Schema.object({
-    a: Schema.number(),
-    b: Schema.number(),
-    c: Schema.number(),
-  }),
-  f: Schema.object({
-    a: Schema.number(),
-    b: Schema.number(),
-  }),
+  onedice: Schema.intersect([
+    Schema.object({
+      random: Schema.function().description('随机数生成函数。').hidden(),
+      maxRollCount: Schema.number().description('单次投掷最大投掷数量。').min(0).step(1),
+    }),
+    Schema.object({
+      d: Schema.object({
+        a: Schema.number().description('掷出多少个骰子'),
+        b: Schema.number().description('掷出的骰子有多少面'),
+        c: Schema.number().description('符号 k/q, 只选取最大/小骰子结果的个数, k 大 q 小'),
+        d: Schema.number().description('符号 p/b, 追加奖惩骰的个数'),
+        e: Schema.number().description('符号 a, 功能转换为骰池，结果转为记录不低于该值的骰子个数'),
+      }).description('普通多面掷骰'),
+      p: Schema.object({
+        // a: Schema.number().description('无意义'),
+        b: Schema.number().description('追加奖惩骰的个数'),
+      }).description('惩罚骰/奖励骰'),
+      a: Schema.object({
+        a: Schema.number().description('第一轮掷出多少个骰子'),
+        b: Schema.number().description('每有一枚不低于该值，则下一轮掷骰增加一枚骰子'),
+        c: Schema.number().description('符号 k, 最终计算不低于该参数的骰子个数'),
+        d: Schema.number().description('符号 q, 最终计算不高于该参数的骰子个数'),
+        e: Schema.number().description('符号 m, 掷出的骰子有多少面'),
+      }).description('无限加骰池'),
+      c: Schema.object({
+        a: Schema.number().description('第一轮掷出多少个骰子'),
+        b: Schema.number().description('每有一枚不低于该值，则下一轮掷骰增加一枚骰子'),
+        c: Schema.number().description('符号 m, 掷出的骰子有多少面'),
+      }).description('双重十字加骰池'),
+      f: Schema.object({
+        a: Schema.number().description('掷出多少个骰子'),
+        // b: Schema.number().description('无意义'),
+      }).description('FATE 掷骰池'),
+    }).description('骰子表达式默认值'),
+  ]),
 })
 
 
@@ -48,7 +53,8 @@ export function onedice(ctx: Context, config: Config) {
   ctx.middleware((session, next) => {
     const { content, prefix, appel } = session.parsed
     if (prefix === null && !appel) return next()
-    const res = /^(rh?|ww?|r?dx?)(.*)$/.exec(content)
+    const noprefix = prefix ? content.substring(prefix.length) : content
+    const res = /^(rh?|ww?|r?dx?)(.*)$/.exec(noprefix)
     if (!res) return next()
     const [, type, raw] = res
     const args = raw.trim().split(/\s+/)
@@ -73,12 +79,12 @@ export function onedice(ctx: Context, config: Config) {
       const diceExpression = (splited.length === 2 ? splited[1] : splited[0]).toLowerCase()
       if (!Number.isFinite(times) || times > config.maxRollTimes)
         return '请输入正确的投掷次数。'
-      const name = session.user.name || session.author.nickname || session.author.username
+      const name = session.username
       const left = reason
         ? `${name} 因为 ${reason} 投掷:\n`
         : `${name} 投掷:\n`
       try {
-        const dices = new Array(times).fill(0).map(_ => dice(diceExpression, config))
+        const dices = new Array(times).fill(0).map(_ => dice(diceExpression, config.onedice))
         let message: string
         if (times === 1) {
           const [[value, root]] = dices
